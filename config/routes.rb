43f0c4ec35f4225d590d208a8bca4417e736208b
Yazima <yazima04200@gmail.com>
2022-09-07 23:27:34 +0900

    ユーザー新規登録追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
